507883850deea270960f7a0d99fc984e1fe7b2dc
Lev <lev@MacBook-Air.staffnet>
2022-11-28 18:13:53 -0500

    initial

--- utils/validators.js
+++ utils/validators.js
@@ -18,7 +18,7 @@ exports.registerValidators = [
         })
     .normalizeEmail(),
     body('password', 'Password must be at least 8 characters')
-        .isLength({min: 8, max: 56})
+        .isLength({min: 1, max: 56})
         .isAlphanumeric()
         .trim(),
     body('confirm')
